d00c03ec832c4fd760275e5927bbf115485a4944
Irina Gudova <melioraa@gmail.com>
2015-04-11 18:22:31 +0300

    Resume

---
+++ Resume/Resume/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controls.Add(button1);
+            MessageBox.Show("Образование");
+            MessageBox.Show("Опыт");
+            MessageBox.Show("Контакты");
+        }
+    }
+}
